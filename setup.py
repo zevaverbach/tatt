355327afd7e128e944e58216f71be9cf28b05dbe
zevav <zev@averba.ch>
2019-02-12 20:18:08 -0500

    added descriptions to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
 
 
+with open('README.md') as file:
+    long_description = file.read()
+
 setup(
     name="tatt",
     version="0.1",
@@ -13,6 +16,9 @@ setup(
         ],
     include_package_data=True,
     packages=find_packages(),
+    description=('Tatt creates a uniform API for multiple speech-to-text '
+                 '(STT) services.'),
+    long_description=long_description,
     entry_points='''
         [console_scripts]
         transcribe=tatt.transcribe:cli
