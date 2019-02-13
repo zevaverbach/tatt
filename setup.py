1ba60b0df20194ea8361206d86f26f5bfc4d546d
zevav <zev@averba.ch>
2019-02-12 21:09:37 -0500

    bumped setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as file:
 
 setup(
     name="tatt",
-    version="0.12",
+    version="0.13",
     py_modules=['tatt'],
     url='https://github.com/zevaverbach/tatt',
     install_requires=[
